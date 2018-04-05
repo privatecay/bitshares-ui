50d36db2437a37e5111d9db33a624e364107741c
Bill Butler <bill@butler.net>
2018-04-04 22:23:18 -0500

    merge with upstream

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 # This script merges upstream/staging into origin staging and deploys to S3
 git checkout staging
 git pull upstream staging
-# git rebase upstream/staging
 git commit -am "merge with upstream"
 git push origin staging
