beee25c308662c5327796fef220169b613e774dc
Bill Butler <bill@butler.net>
2018-04-18 11:43:16 -0500

    merge with upstream

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 # This script merges upstream/staging into origin staging and deploys to S3
+# You must define upstream repo by running this command:
+# git remote add upstream git@github.com:bitshares/bitshares-ui.git
 git checkout staging
+git pull origin staging
 git pull upstream staging
 git commit -am "merge with upstream"
 git push origin staging
